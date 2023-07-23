c292eaa73f664729c35f7efb6d4e8b5c7eab7beb
TheMetaSetter <134222962+TheMetaSetter@users.noreply.github.com>
2023-07-23 21:54:07 +0700

    Add comment in 'main.cpp'

--- main.cpp
+++ main.cpp
@@ -200,8 +200,8 @@ int main(int argc, const char *argv[])
                 // Print result
                 PrintResult(argv[5], runningTime, countCompare);
 
-                // Write experimental results to .csv file
-                WriteExperimentalResultToCsvFile(algoName, orderIdx, size, runningTime, countCompare);
+                // // Write experimental results to .csv file
+                // WriteExperimentalResultToCsvFile(algoName, orderIdx, size, runningTime, countCompare);
 
                 std::cout << "\n\n";
 
